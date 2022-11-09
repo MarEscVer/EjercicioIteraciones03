770eb78d52c881f46834e56df0ce4666859785ee
Maria <maria_escribanoverde@gmail.com>
2022-11-09 16:06:19 +0100

    FINAL

--- 03/src/unico/Ejercicio03.java
+++ 03/src/unico/Ejercicio03.java
@@ -7,6 +7,15 @@ public class Ejercicio03 {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		
+/* Realizar un aplicación que genere un número aleatorio entre 1 y 100.
+ * El jugador debe ir probando números tratando de acertarlo.
+ * El programa debe indicar "mayor" o "menor" según el número secreto
+ * sea mayor o menor que el introducido por el usuario. El proceso finaliza
+ * cuando el usuario acierta o cuando se rinde (introduciendo un -1).
+ */
+
+
+		
 		int random;
 		int num;
 		final int RENDIRSE = -1;
